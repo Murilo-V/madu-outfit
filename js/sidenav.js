1d59e2860b4c383ce34b8812d0ca77d89797d115
Murilo-V <alveiramurilo@hotmail.com>
2020-09-06 17:19:19 -0300

    feat: slideshow

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,14 +1,11 @@
 function openNav() {
-    const container =  document.getElementById("container-fluid");
     document.getElementById("sidenav").style.width = "250px";
-    container.style.marginLeft = "250px";
+    document.getElementById("container-fluid").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0, 54, 104,0.5)";
   }
   
-  
   function closeNav() {
-    const container =  document.getElementById("container-fluid");
     document.getElementById("sidenav").style.width = "0";
-    container.style.marginLeft = "0";
+    document.getElementById("container-fluid").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
   }
