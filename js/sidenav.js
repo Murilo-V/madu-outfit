677f637907d5f9f23176d7c9632bfd5ed60768d9
Murilo-V <alveiramurilo@hotmail.com>
2020-09-12 17:22:31 -0300

    fix: overflow bug

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,11 +1,21 @@
+const sidenav = document.getElementById('sidenav');
+const container = document.getElementById('container-fluid');
+
+document.body.style.transition = '500ms';
+document.body.style.transitionProperty = 'background-color';
+
 function openNav() {
-    document.getElementById("container-fluid").style.marginLeft = "250px";
-    document.getElementById("sidenav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0, 54, 104,0.5)";
+    container.style.marginLeft = '250px';
+    sidenav.style.width = '250px';
+    document.body.style.overflow = 'hidden'
+    document.body.style.backgroundColor = 'rgba(0, 54, 104,0.5)';
+    window.scrollTo(0,0);
   }
   
-  function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("container-fluid").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+function closeNav() {
+    sidenav.style.width = '0';
+    container.style.marginLeft = '0';
+    document.body.style.overflow = 'visible'
+    document.body.style.backgroundColor = 'white';
+    window.scrollTo(0,0);
   }
